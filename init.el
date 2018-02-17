78c5f14d3e6c74cb0e49dff2b102805593b75cad
DanHodges <dhdgs690@gmail.com>
2018-02-16 21:45:16 -0600

    pretty format init file

--- init.el
+++ init.el
@@ -5,48 +5,71 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
+(unless (package-installed-p 'use-package) 
+  (package-refresh-contents) 
   (package-install 'use-package))
 
-(eval-when-compile
-(require 'use-package))
+(eval-when-compile 
+  (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
 
 (setq use-package-always-ensure t)
 
-(use-package exec-path-from-shell
-  :config
-  (exec-path-from-shell-initialize))
+(use-package 
+  exec-path-from-shell 
+  :config (exec-path-from-shell-initialize))
 
-(use-package which-key
-  :init
-  (which-key-mode))
-(use-package beacon
-  :init
-  (beacon-mode 1))
-(use-package rjsx-mode
+(use-package 
+  which-key 
+  :init (which-key-mode))
+
+(use-package 
+  beacon 
+  :init (beacon-mode 1))
+
+(use-package 
+  rjsx-mode 
   :mode "\\.js\\'")
-(use-package material-theme
+
+(use-package 
+  material-theme 
   :defer t)
 (load-theme 'material t)
 
-(use-package projectile
-  :init
-  (projectile-mode))
+(use-package 
+  projectile 
+  :init (projectile-mode))
 (setq projectile-completion-system 'ivy)
 
-(use-package ivy
-  :init
-  (ivy-mode 1))
-(use-package counsel)
-(use-package counsel-projectile)
-(use-package flycheck)
-(use-package lsp-mode)
-(use-package lsp-javascript-typescript
-  :init
-  (add-hook 'rjsx-mode-hook #'lsp-javascript-typescript-enable))
+(use-package 
+  ivy 
+  :init (ivy-mode 1))
+
+(use-package 
+  counsel)
+
+(use-package 
+  counsel-projectile)
+
+(use-package 
+  flycheck)
+
+(use-package 
+  lsp-mode)
+
+(use-package 
+  lsp-javascript-typescript 
+  :init (add-hook 'rjsx-mode-hook #'lsp-javascript-typescript-enable))
+(use-package 
+  wgrep)
+
+(use-package 
+  elisp-format)
+
+(setq wgrep-auto-save-buffer t)
+(setq wgrep-enable-key "r")
+(setq wgrep-change-readonly-file t)
 
 (setq ivy-use-virtual-buffers t)
 (setq enable-recursive-minibuffers t)
@@ -57,12 +80,8 @@
 (setq mac-command-modifier 'super)
 (setq ring-bell-function 'ignore)
 
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
 (when window-system  (global-hl-line-mode 1))
 (when window-system  (global-prettify-symbols-mode t))
@@ -71,8 +90,7 @@
 (menu-bar-mode 1)
 (scroll-bar-mode -1)
 (global-visual-line-mode 1)
-;;(desktop-save-mode 1)
-
+(desktop-save-mode 1)
 
 (global-set-key "\C-s" 'swiper)
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
@@ -91,6 +109,7 @@
 (global-set-key (kbd "C-c k") 'counsel-ag)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+
 (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)
 
 
@@ -98,8 +117,16 @@
 (global-set-key (kbd "s-\\") 'split-window-right)
 (global-set-key (kbd "s-w") 'delete-window)
 (global-set-key (kbd "s-W") 'delete-frame)
-(global-set-key (kbd "s-[") (lambda () (interactive) (other-window -1)))
-(global-set-key (kbd "s-]") (lambda () (interactive) (other-window 1)))
+(global-set-key (kbd "s-[") 
+		(lambda () 
+		  (interactive) 
+		  (other-window -1)))
+
+(global-set-key (kbd "s-]") 
+		(lambda () 
+		  (interactive) 
+		  (other-window 1)))
+
 (global-set-key (kbd "s-i") 'projectile-find-tag)
 (global-set-key (kbd "s-g") 'counsel-git-grep)
 (global-set-key (kbd "s-o") 'projectile-switch-project)
@@ -110,15 +137,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(lsp-project-whitelist
-   (quote
-    ("^/Users/dxhodge/code/MyWsb/$" "^/Users/dxhodge/code/hawaii-react-js/$")))
+ '(ansi-color-faces-vector [default default default italic underline success warning error]) 
+ '(lsp-project-whitelist (quote ("^/Users/dxhodge/code/MyWsb/$"
+				 "^/Users/dxhodge/code/hawaii-react-js/$"))) 
  '(package-selected-packages (quote (counsel-projectile use-package diminish))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 145 :width normal :foundry "nil" :family "Source Code Pro")))))
+ '(default ((t 
+	     (:inherit nil 
+		       :stipple nil 
+		       :background "#263238" 
+		       :foreground "#ffffff" 
+		       :inverse-video nil 
+		       :box nil 
+		       :strike-through nil 
+		       :overline nil 
+		       :underline nil 
+		       :slant normal 
+		       :weight normal 
+		       :height 145 
+		       :width normal 
+		       :foundry "nil" 
+		       :family "Source Code Pro")))))
